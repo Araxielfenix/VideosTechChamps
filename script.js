762e5d319392635ca6296ed9929fefc9e2d64628
Araxielfenix <araxielfenix@outlook.com>
2022-06-07 15:46:49 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,30 +3,63 @@ window.onload = function () {
     document.getElementById("videosShopify").style.display = "none";
     document.getElementById("videosWC").style.display = "none";
     document.getElementById("videosPS").style.display = "none";
+    document.getElementById("videosLeft").style.display = "none";
 }
 
 function showShopifyVideos() {
-    if(document.getElementById("videosShopify").style.display == "none"){
-        document.getElementById("videosShopify").style.display = "block";
+    if(document.getElementById("videosShopify").style.display == "none" && document.getElementById("videosLeft").style.display == "none"){
+        document.getElementById("videosShopify").style.display = "grid";
+        document.getElementById("videosWC").style.display = "none";
+        document.getElementById("videosPS").style.display = "none";
+
+        document.getElementById("videosLeft").style.display = "grid";
+        document.getElementById("videosCenter").style.display = "none";
     }
-    else if (document.getElementById("videosShopify").style.display == "block"){
+    else if (document.getElementById("videosShopify").style.display == "grid" && document.getElementById("videosLeft").style.display == "grid"){
         document.getElementById("videosShopify").style.display = "none";
+
+        document.getElementById("videosLeft").style.display = "none";
+        document.getElementById("videosCenter").style.display = "grid";
     }
 }
-
 function showWCVideos() {
-    if(document.getElementById("videosWC").style.display == "none"){
-        document.getElementById("videosWC").style.display = "block";
+    if(document.getElementById("videosWC").style.display == "none" && document.getElementById("videosLeft").style.display == "none"){
+        document.getElementById("videosShopify").style.display = "none";
+        document.getElementById("videosWC").style.display = "grid";
+        document.getElementById("videosPS").style.display = "none";
+
+        document.getElementById("videosLeft").style.display = "grid";
+        document.getElementById("videosCenter").style.display = "none";
     }
-    else if (document.getElementById("videosWC").style.display == "block"){
+    else if (document.getElementById("videosWC").style.display == "grid" && document.getElementById("videosLeft").style.display == "grid"){
         document.getElementById("videosWC").style.display = "none";
+
+        document.getElementById("videosLeft").style.display = "none";
+        document.getElementById("videosCenter").style.display = "grid";
     }
 }
 function showPSVideos(){
-    if(document.getElementById("videosPS").style.display == "none"){
-        document.getElementById("videosPS").style.display = "block";
+    if(document.getElementById("videosPS").style.display == "none" && document.getElementById("videosLeft").style.display == "none"){
+        document.getElementById("videosShopify").style.display = "none";
+        document.getElementById("videosWC").style.display = "none";
+        document.getElementById("videosPS").style.display = "grid";
+
+        document.getElementById("videosLeft").style.display = "grid";
+        document.getElementById("videosCenter").style.display = "none";
     }
-    else if (document.getElementById("videosPS").style.display == "block"){
+    else if (document.getElementById("videosPS").style.display == "grid" && document.getElementById("videosLeft").style.display == "grid"){
         document.getElementById("videosPS").style.display = "none";
+
+        document.getElementById("videosLeft").style.display = "none";
+        document.getElementById("videosCenter").style.display = "grid";
     }
+}
+
+//copy to clipboard input value.
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).val()).select();
+    document.execCommand("copy");
+    $temp.remove();
 }
